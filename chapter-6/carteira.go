918744eaee0a424dda4e179830d639da4b611321
joaquim-almeida <joaquimrafael30@hotmail.com>
2022-03-02 23:59:40 -0300

    chapter 6

---
+++ chapter-6/carteira.go
@@ -0,0 +1,38 @@
+package carteira
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErroSaldoInsuficiente = errors.New("não é possivel retirar: saldo insuficiente")
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Carteira struct {
+	saldo Bitcoin
+}
+
+func (c *Carteira) Depositar(quantidade Bitcoin) {
+	c.saldo += quantidade
+}
+
+func (c *Carteira) Saldo() Bitcoin {
+	return c.saldo
+}
+
+func (c *Carteira) Retirar(quantidade Bitcoin) error {
+	if quantidade > c.saldo {
+		return ErroSaldoInsuficiente
+	}
+	c.saldo -= quantidade
+	return nil
+}
---
+++ chapter-6/carteira_test.go
@@ -0,0 +1,49 @@
+package carteira
+
+import (
+	"testing"
+)
+
+func TestCarteira(t *testing.T) {
+	t.Run("Depositar", func(t *testing.T) {
+		carteira := Carteira{}
+		carteira.Depositar(Bitcoin(10))
+
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
+
+	t.Run("Retirar", func(t *testing.T) {
+		carteira := Carteira{saldo: Bitcoin(20)}
+		carteira.Retirar(Bitcoin(10))
+
+		confirmaSaldo(t, carteira, Bitcoin(10))
+	})
+
+	t.Run("Retirar com saldo insuficiente", func(t *testing.T) {
+		saldoInicial := Bitcoin(20)
+		carteira := Carteira{saldoInicial}
+		erro := carteira.Retirar(Bitcoin(100))
+
+		confirmaSaldo(t, carteira, saldoInicial)
+		confirmaErro(t, erro, ErroSaldoInsuficiente)
+	})
+}
+
+func confirmaSaldo(t *testing.T, carteira Carteira, esperado Bitcoin) {
+	t.Helper()
+	resultado := carteira.Saldo()
+	if resultado != esperado {
+		t.Errorf("resultado '%s' esperado '%s'", resultado, esperado)
+	}
+}
+
+func confirmaErro(t *testing.T, resultado error, esperado error) {
+	t.Helper()
+	if resultado == nil {
+		t.Fatal("Esperava um erro, mas nenhum ocorreu")
+	}
+
+	if resultado != esperado {
+		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	}
+}
